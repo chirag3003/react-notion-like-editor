d7ee7b5a48d23134960212603f3cf9ac549ca5b2
Chirag Bhalotia <chiragbhalotia0412@gmail.com>
2024-01-06 17:18:08 +0530

    added drag button

--- src/Editor/EditorInput.tsx
+++ src/Editor/EditorInput.tsx
@@ -1,45 +1,54 @@
-import {Value} from "../types";
+import { Value } from "../types";
+import { GripVertical } from "lucide-react";
 
 interface EditorInputProps {
-    value: Value;
-    index: number;
-    onEnter: (index: number) => void;
-    onDelete: (index: number) => void;
-    changeValue: (index: number, value: string) => void;
+  value: Value;
+  index: number;
+  onEnter: (index: number) => void;
+  onDelete: (index: number) => void;
+  changeValue: (index: number, value: string) => void;
 }
 
-function EditorInput({value, onEnter, index, changeValue,onDelete}: EditorInputProps) {
-    return (
-        <div className="w-full border border-gray-100 p-2 shadow input-container">
-            <div
-                onBeforeInput={(e) => {
-                    const evt = e.nativeEvent as KeyboardEvent;
-                    if (evt.key === "Enter") {
-                        if (!evt.shiftKey) {
-                            evt.preventDefault();
-                            onEnter(index);
-                        }
-                    }
-                }}
-                onKeyDown={e => {
-                    if (e.key !== "Backspace") return;
-                    if(value.value !== "") return;
-                    e.preventDefault()
-                    onDelete(index)
-                }}
-
-                onInput={(e) => {
-                    const text = e.currentTarget.textContent;
-                    changeValue(index, text || "");
-                }}
-                className="w-full text-5xl font-bold outline-none input "
-                contentEditable={true}
-                dangerouslySetInnerHTML={{__html: value.value}}
-            >
-                {/* {value.value} */}
-            </div>
-        </div>
-    );
+function EditorInput({
+  value,
+  onEnter,
+  index,
+  changeValue,
+  onDelete,
+}: EditorInputProps) {
+  return (
+    <div className="w-full border border-gray-100 p-2 shadow input-container flex gap-5 group">
+      <button className="dragButton invisible group-hover:visible ">
+        <GripVertical height={25} width={25} className="h-full text-gray-500" />
+      </button>
+      <div
+        onBeforeInput={(e) => {
+          const evt = e.nativeEvent as KeyboardEvent;
+          if (evt.key === "Enter") {
+            if (!evt.shiftKey) {
+              evt.preventDefault();
+              onEnter(index);
+            }
+          }
+        }}
+        onKeyDown={(e) => {
+          if (e.key !== "Backspace") return;
+          if (value.value !== "") return;
+          e.preventDefault();
+          onDelete(index);
+        }}
+        onInput={(e) => {
+          const text = e.currentTarget.textContent;
+          changeValue(index, text || "");
+        }}
+        className="w-full text-5xl font-bold outline-none input "
+        contentEditable={true}
+        dangerouslySetInnerHTML={{ __html: value.value }}
+      >
+        {/* {value.value} */}
+      </div>
+    </div>
+  );
 }
 
 export default EditorInput;
